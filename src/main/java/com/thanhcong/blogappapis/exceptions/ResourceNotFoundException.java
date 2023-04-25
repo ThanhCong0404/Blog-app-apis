e799a084b0e6bd4f01f5fb3bc2ed17667e28249b
ThanhCong <thanhcong04042001@gmail.com>
2023-04-25 15:04:59 +0700

    handling exception

--- src/main/java/com/thanhcong/blogappapis/exceptions/ResourceNotFoundException.java
+++ src/main/java/com/thanhcong/blogappapis/exceptions/ResourceNotFoundException.java
@@ -5,14 +5,14 @@ import lombok.Setter;
 
 @Getter
 @Setter
-public class ResourceNotFoundException extends RuntimeException{
+public class ResourceNotFoundException extends RuntimeException {
     String resourceName;
     String fieldName;
     long fieldValue;
 
 
     public ResourceNotFoundException(String resourceName, String fieldName, long fieldValue) {
-        super(String.format("%s not found with %s : %l",resourceName,fieldName,fieldName));
+        super(String.format("%s not found with %s : %d", resourceName, fieldName, fieldValue));
         this.resourceName = resourceName;
         this.fieldName = fieldName;
         this.fieldValue = fieldValue;
